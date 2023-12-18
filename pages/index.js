48cf34c7923f71afa7f13c52b918ac0b9a55e2df
niranjancr7 <152554795+niranjancr7@users.noreply.github.com>
2023-12-18 23:38:07 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
-import {useState, useEffect} from "react";
-import {ethers} from "ethers";
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import QRCode from 'qrcode.react';
+import { format } from 'date-fns';
 import atm_abi from "../artifacts/contracts/Assessment.sol/Assessment.json";
 
 export default function HomePage() {
@@ -7,41 +9,42 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [transactionHistory, setTransactionHistory] = useState([]);
+  const [depositAmount, setDepositAmount] = useState(0);
+  const [withdrawAmount, setWithdrawAmount] = useState(0);
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
 
-  const getWallet = async() => {
+  const getWallet = async () => {
     if (window.ethereum) {
       setEthWallet(window.ethereum);
     }
 
     if (ethWallet) {
-      const account = await ethWallet.request({method: "eth_accounts"});
+      const account = await ethWallet.request({ method: "eth_accounts" });
       handleAccount(account);
     }
-  }
+  };
 
   const handleAccount = (account) => {
     if (account) {
-      console.log ("Account connected: ", account);
+      console.log("Account connected: ", account);
       setAccount(account);
-    }
-    else {
+    } else {
       console.log("No account found");
     }
-  }
+  };
 
-  const connectAccount = async() => {
+  const connectAccount = async () => {
     if (!ethWallet) {
       alert('MetaMask wallet is required to connect');
       return;
     }
-  
+
     const accounts = await ethWallet.request({ method: 'eth_requestAccounts' });
     handleAccount(accounts);
-    
-    // once wallet is set we can get a reference to our deployed contract
+
     getATMContract();
   };
 
@@ -49,44 +52,70 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
-    setATM(atmContract);
-  }
 
-  const getBalance = async() => {
+    setATM(atmContract);
+  };
+
+  const getBalance = async () => {
     if (atm) {
       setBalance((await atm.getBalance()).toNumber());
     }
-  }
+  };
 
-  const deposit = async() => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
-    }
-  }
+  const updateTransactionHistory = (type, amount) => {
+    const transactionType = type === 'deposit' ? 'CR' : 'DR';
+    const transaction = {
+      type: transactionType,
+      amount,
+      date: new Date(),
+    };
 
-  const withdraw = async() => {
+    setTransactionHistory(prev => [transaction, ...prev.slice(0, 4)]);
+  };
+
+  const deposit = async () => {
     if (atm) {
-      let tx = await atm.withdraw(1);
-      await tx.wait()
+      let tx = await atm.deposit(depositAmount);
+      await tx.wait();
       getBalance();
+      updateTransactionHistory('deposit', depositAmount);
     }
-  }
+  };
+
+  const withdraw = async () => {
+    if (atm) {
+      let tx = await atm.withdraw(withdrawAmount);
+      await tx.wait();
+      getBalance();
+      updateTransactionHistory('withdraw', withdrawAmount);
+    }
+  };
+
+  const miniStatement = () => (
+    <div>
+      <h2>Mini Statement</h2>
+      <p>Scan this QR code to view mini statement</p>
+      <ul>
+        {transactionHistory.map((transaction, index) => (
+          <li key={index}>
+            {index + 1}. {transaction.type} - {transaction.amount} ETH - {transaction.date.toString()}
+          </li>
+        ))}
+      </ul>
+      <QRCode value={JSON.stringify(transactionHistory)} />
+    </div>
+  );
 
   const initUser = () => {
-    // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <p>Please install Metamask in order to use this ATM.</p>;
     }
 
-    // Check to see if user is connected. If not, connect to their account
     if (!account) {
-      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>
+      return <button onClick={connectAccount}>Please connect your Metamask wallet</button>;
     }
 
-    if (balance == undefined) {
+    if (balance === undefined) {
       getBalance();
     }
 
@@ -94,24 +123,54 @@ export default function HomePage() {
       <div>
         <p>Your Account: {account}</p>
         <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <div className="user-actions">
+          <input
+            type="number"
+            value={depositAmount}
+            onChange={(e) => setDepositAmount(parseInt(e.target.value))}
+            placeholder="Enter deposit amount"
+          />
+          <button onClick={deposit}>Deposit</button>
+          <input
+            type="number"
+            value={withdrawAmount}
+            onChange={(e) => setWithdrawAmount(parseInt(e.target.value))}
+            placeholder="Enter withdrawal amount"
+          />
+          <button onClick={withdraw}>Withdraw</button>
+        </div>
       </div>
-    )
-  }
+    );
+  };
 
-  useEffect(() => {getWallet();}, []);
+  useEffect(() => { getWallet(); }, []);
 
   return (
     <main className="container">
       <header><h1>Welcome to the Metacrafters ATM!</h1></header>
       {initUser()}
+      {miniStatement()}
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
+          background-color: #ffa500; /* Orange background color */
+          padding: 20px;
+        }
+
+        .user-actions {
+          margin-top: 10px;
+        }
+
+        input {
+          margin-right: 10px;
+          padding: 5px;
+        }
+
+        button {
+          padding: 5px;
         }
       `}
       </style>
     </main>
-  )
+  );
 }
