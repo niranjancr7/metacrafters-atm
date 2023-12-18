c499f288fd437f762f3b0e76e6a4f161c2da736f
niranjancr7 <152554795+niranjancr7@users.noreply.github.com>
2023-12-18 23:37:33 +0530

    Update Assessment.sol

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -1,60 +1,65 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-//import "hardhat/console.sol";
-
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    uint256 public interestRate; 
+    bool public isAccountLocked;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event InterestAccrued(uint256 amount);
+    event AccountLocked();
+    event AccountUnlocked();
 
-    constructor(uint initBalance) payable {
-        owner = payable(msg.sender);
-        balance = initBalance;
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
     }
 
-    function getBalance() public view returns(uint256){
+    modifier notLocked() {
+        require(!isAccountLocked, "Account is locked");
+        _;
+    }
+
+    constructor(uint initBalance) payable {
+    owner = payable(msg.sender);
+    balance = initBalance;
+    interestRate = 0; 
+    isAccountLocked = false;
+}
+
+
+    function getBalance() public view returns (uint256) {
         return balance;
     }
 
-    function deposit(uint256 _amount) public payable {
-        uint _previousBalance = balance;
-
-        // make sure this is the owner
-        require(msg.sender == owner, "You are not the owner of this account");
-
-        // perform transaction
-        balance += _amount;
-
-        // assert transaction completed successfully
-        assert(balance == _previousBalance + _amount);
-
-        // emit the event
-        emit Deposit(_amount);
+    function deposit() public payable onlyOwner notLocked {
+        balance += msg.value;
+        emit Deposit(msg.value);
     }
 
-    // custom error
-    error InsufficientBalance(uint256 balance, uint256 withdrawAmount);
-
-    function withdraw(uint256 _withdrawAmount) public {
-        require(msg.sender == owner, "You are not the owner of this account");
-        uint _previousBalance = balance;
-        if (balance < _withdrawAmount) {
-            revert InsufficientBalance({
-                balance: balance,
-                withdrawAmount: _withdrawAmount
-            });
-        }
-
-        // withdraw the given amount
+    function withdraw(uint256 _withdrawAmount) public onlyOwner notLocked {
+        require(_withdrawAmount <= balance, "Insufficient balance");
         balance -= _withdrawAmount;
-
-        // assert the balance is correct
-        assert(balance == (_previousBalance - _withdrawAmount));
-
-        // emit the event
+        payable(owner).transfer(_withdrawAmount);
         emit Withdraw(_withdrawAmount);
+    }
+
+    function reinvestInterest() public onlyOwner notLocked {
+        uint256 interest = (balance * interestRate) / 100;
+        balance += interest;
+        emit InterestAccrued(interest);
+    }
+
+    function lockAccount() public onlyOwner {
+        isAccountLocked = true;
+        emit AccountLocked();
+    }
+
+    function unlockAccount() public onlyOwner {
+        isAccountLocked = false;
+        emit AccountUnlocked();
     }
 }
